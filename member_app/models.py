be68eb790808b82d46ae10d9651d9ec449510b3d
louwiee <126698636+louwiee@users.noreply.github.com>
2023-04-01 15:11:14 +0800

    Update models.py

--- member_app/models.py
+++ member_app/models.py
@@ -3,10 +3,15 @@ from django.conf import settings
 from django.contrib.auth.models import User
 # Create your models here.
 class MemberProfile(models.Model):
-  user = models.OneToOneField(User, on_delete=models.CASCADE)
-  about = models.CharField(max_length=150, default="I am a customer", null=True)
-  address = models.CharField(max_length=500, null=True)
-  credit_rating = models.IntegerField(null=True)
+    MEM_NUM = models.IntegerField(primary_key=True, ) 
+    MEM_FNAME = models.CharField(max_length=300, null=True)  
+    MEM_LNAME = models.CharField(max_length=300, null=True) 
+    MEM_STREET = models.CharField(max_length=300, null=True)
+    MEM_CITY = models.CharField(max_length=300, null=True)
+    MEM_STATE = models.CharField(max_length=300, null=True)
+    MEM_ZIP = models.CharField(max_length=300, null=True)
+    MEM_BALANCE = models.CharField(max_length=300, null=True)
 
-  def __str__(self):
+
+def __str__(self):
     return str(self.user)
