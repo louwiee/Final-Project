e783cfafe3495f1e4322c213d30b57e71f49dd8d
louwiee <126698636+louwiee@users.noreply.github.com>
2023-04-01 15:09:35 +0800

    Update urls.py

--- rental_app/urls.py
+++ rental_app/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from rental_app import views
 
 
+    
 urlpatterns = [
+    
     path("home", views.home, name="home"),
-    path("addrent/<str:pk>", views.addrent, name="addrent")
+    path("addrent/<str:pk>", views.addrent, name="addrent"),
+    path("Memberprofile/<str:pk>", views.profile_page, name="memberprofile"),
+    path('rent/<int:movie_id>/', views.rent_movie, name='rent_movie'),
+    path('return/<int:rental_id>', views.return_movie, name='return_movie'),
+    
     
 ]
